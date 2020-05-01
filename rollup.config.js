7638b840083951656013fc2e6aa65745f5e66d59
Radek Novak <radek.novak.cze@gmail.com>
2020-05-01 12:01:39 +0200

    rollup config input file

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,30 @@
-import typescript from "rollup-plugin-typescript2";
-import pkg from "./package.json";
-import { terser } from "rollup-plugin-terser";
+import typescript from 'rollup-plugin-typescript2';
+import pkg from './package.json';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
-  input: "src/index.ts", // our source file
+  input: 'src/index.tsx', // our source file
   output: [
     {
       file: pkg.main,
-      format: "cjs",
+      format: 'cjs'
     },
     {
       file: pkg.module,
-      format: "es", // the preferred format
+      format: 'es' // the preferred format
     },
     {
       file: pkg.browser,
-      format: "iife",
+      format: 'iife',
       // the global which can be used in a browser
-      name: "ReactTinyMarkup",
-    },
+      name: 'ReactTinyMarkup'
+    }
   ],
   external: [...Object.keys(pkg.dependencies || {})],
   plugins: [
     typescript({
-      typescript: require("typescript"),
+      typescript: require('typescript')
     }),
-    terser(), // minifies generated bundles
-  ],
+    terser() // minifies generated bundles
+  ]
 };
